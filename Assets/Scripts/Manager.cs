c695dbe9fce6b57ba3326dc8a950a03fe56d3588
Subhamkar <subhamkar1996@gmail.com>
2017-09-18 11:03:11 -0700

    initial

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour {
+
+    public static int currentScore;
+    public static int highScore;
+
+    public static int currentLevel = 0;
+    public static int unlockedLevel;
+
+    public static void CompleteLevel()
+    {
+        if (currentLevel < 2)
+        {
+            print(currentLevel);
+            currentLevel += 1;
+            //Application.LoadLevel(currentLevel);
+            SceneManager.LoadScene(currentLevel);
+        }
+        else
+        {
+            print("You Win");
+        }
+    }
+}
